775d053b0c091256ca280e7942116e1e3ad7f263
Nabil Ferdana <nabilferdana03@gmail.com>
2023-07-06 23:05:16 +0700

    Conditional Rendering

--- src/components/Layout/AuthLayout.jsx
+++ src/components/Layout/AuthLayout.jsx
@@ -18,7 +18,7 @@ const Navigation = ({type}) => {
   if (type === 'login') {
     return (
       <p className="text-sm mt-5 text-center">
-        Don't have an account?{" "}
+        Don't have an account? {" "}
         <Link to="/register" className="font-bold text-blue-600">
             Sign Up
           </Link>
@@ -27,7 +27,7 @@ const Navigation = ({type}) => {
   } else {
     return (
       <p className="text-sm mt-5 text-center">
-        Already have an account?{' '}
+        Already have an account? {' '}
         <Link to="/login" className="font-bold text-blue-600">
           Sign In
         </Link>
