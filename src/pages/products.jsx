38f7592ce9f912265af11c941f617c96e54bf48c
Nabil Ferdana <nabilferdana03@gmail.com>
2023-07-07 22:33:44 +0700

    Rendering List

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,25 +1,30 @@
-import Button from "../components/Elements/Button";
-import CardProducts from "../components/Fragment/CardProducts";
+import CardProducts from '../components/Fragment/CardProducts';
 
-const Products = () => {
+const product = [
+  {
+    id: 1,
+    name: 'Sepatu Baru',
+    price: '1.000.000',
+    image: '/images/shoes-1.jpg',
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Sunt nostrum cupiditate ad ab veritatis, natus totam facilis facere corrupti soluta tenetur 
+      perspiciatis minima illo veniam. 
+      Placeat doloremque inventore temporibus quae.`,
+  },
+];
+
+const ProductsPage = () => {
   return (
     <div className="flex justify-center py-5">
-      <CardProducts>
-        <CardProducts.Header image="/images/shoes-1.jpg" />
-        <CardProducts.Body title="sepatu baru">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt nostrum cupiditate ad ab veritatis, natus totam facilis facere corrupti soluta tenetur perspiciatis minima illo veniam. Placeat doloremque inventore temporibus quae.
-        </CardProducts.Body>
-        <CardProducts.Footer price="Rp.1000,000" />
-      </CardProducts>
-      <CardProducts>
-        <CardProducts.Header image="/images/shoes-1.jpg" />
-        <CardProducts.Body title="sepatu baru">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt nostrum cupiditate ad ab veritatis, natus totam facilis facere corrupti soluta tenetur perspiciatis minima illo veniam. Placeat doloremque inventore temporibus quae.
-        </CardProducts.Body>
-        <CardProducts.Footer price="Rp.1000,000" />
-      </CardProducts>
+      {product.map((product) => (
+        <CardProducts>
+          <CardProducts.Header image={product.image} />
+          <CardProducts.Body name={product.name}>{product.description}</CardProducts.Body>
+          <CardProducts.Footer price={product.price} />
+        </CardProducts>
+      ))}
     </div>
   );
 };
 
-export default Products;
+export default ProductsPage;
